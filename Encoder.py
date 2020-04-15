024cc4d970ea6b81330f48b7d6dbb7e81888d415
liuyuex97 <58824452+liuyuex97@users.noreply.github.com>
2020-04-14 20:43:51 -0400

    Add files via upload
    procrastination is real

--- Encoder.py
+++ Encoder.py
@@ -9,106 +9,109 @@ import torch
 from pointnet_util import PointNetSetAbstraction
 
 class Encoder(nn.Module):
-    def __init__(self, num_points):
-        super(Encoder, self).__init__()
-        self.fe1 = FeatureExtractor_1()
-        self.fe2 = FeatureExtractor_2(num_points)
-        self.out_layer = nn.MaxPool2d((1, 2), 1)
+	def __init__(self, num_points):
+		super(Encoder, self).__init__()
+		self.fe1 = FeatureExtractor_1()
+		self.fe2 = FeatureExtractor_2(512)
+		self.out_layer = nn.MaxPool2d((1, 2), 1)
 
-    def forward(self, x):
-        x_down, out_1 = self.fe1(x) # (batch_size, 512, 3) || (batch_size, 1920)
-        out_2 = self.fe2(x_down) # (batch_size, 1920)
-        out = torch.cat((out_1, out_2), 2) # (batch_size, 1920, 2)
-        out = self.out_layer(out) # (batch_size, 1920)
-        return out
+	def forward(self, x):
+		#print(x)
+		x_down, out_1 = self.fe1(x) # (batch_size, 512, 3) || (batch_size, 1920)
+		out_2 = self.fe2(x_down) # (batch_size, 1920)
+		out = torch.cat((out_1, out_2), 2) # (batch_size, 1920, 2)
+		#print('cat', out.shape)
+		out = self.out_layer(out).view(-1, 1920) # (batch_size, 1920)
+		#print('encoder output shape', out.shape)
+		return out
 
-    def downsampling(self, x): # (batch_size, 2048, 3)
-        pass
+	def downsampling(self, x): # (batch_size, 2048, 3)
+		pass
 
 class FeatureExtractor_1(nn.Module):
-    def __init__(self):
-        super(FeatureExtractor_1, self).__init__()
-        self.in_channel = 3
-        self.sa1 = PointNetSetAbstraction(npoint = 1024, radius = 0.2, nsample = 32,
-                                          in_channel = self.in_channel, mlp = [64, 64, 128], group_all = False)
-        self.sa2 = PointNetSetAbstraction(npoint = 512, radius = 0.2, nsample = 32, in_channel = 128 + self.in_channel,
-                                          mlp = [128, 128, 256], group_all = False)
-        self.sa3 = PointNetSetAbstraction(npoint = 128, radius = 0.2, nsample = 32, in_channel = 256 + self.in_channel,
-                                          mlp = [256, 256, 512], group_all = False)
-        self.sa4 = PointNetSetAbstraction(npoint = None, radius = 0.2, nsample = 32, in_channel = 512 + self.in_channel,
-                                          mlp = [512, 512, 1920], group_all = False)
+	def __init__(self):
+		super(FeatureExtractor_1, self).__init__()
+		self.in_channel = 3
+		self.sa1 = PointNetSetAbstraction(npoint = 1024, radius = 0.2, nsample = 32,
+										  in_channel = self.in_channel, mlp = [64, 64, 128], group_all = False)
+		self.sa2 = PointNetSetAbstraction(npoint = 512, radius = 0.2, nsample = 32, in_channel = 128 + self.in_channel,
+										  mlp = [128, 128, 256], group_all = False)
+		self.sa3 = PointNetSetAbstraction(npoint = 128, radius = 0.2, nsample = 32, in_channel = 256 + self.in_channel,
+										  mlp = [256, 256, 512], group_all = False)
+		self.sa4 = PointNetSetAbstraction(npoint = 1, radius = 0.2, nsample = 32, in_channel = 512 + self.in_channel,
+										  mlp = [512, 512, 1920], group_all = False)
 
-    def forward(self, xyz):
-        batch_size, _, _ = xyz.shape
-        norm = None
+	def forward(self, xyz):
+		xyz = xyz.permute(0, 2, 1)
+		batch_size, _, _ = xyz.shape
+		norm = None
+		l1_xyz, l1_points = self.sa1(xyz, norm)
+		#print('xyz after sa1', l1_xyz.shape, l1_points.shape)
+		l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
 
-        l1_xyz, l1_points = self.sa1(xyz, norm)
-        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
-        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
-        l4_xyz, l4_points = self.sa4(l3_xyz, l3_points)
+		#print('xyz after sa2', l2_xyz.shape, l2_points.shape)
+		l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
 
-        output = l4_points.view(batch_size, 1920)
+		#print('xyz after sa3', l3_xyz.shape, l3_points.shape)
+		l4_xyz, l4_points = self.sa4(l3_xyz, l3_points)
 
-        return l2_xyz, output # (batch_size, 1920)
+		#print('xyz after sa4', l4_xyz.shape, l4_points.shape)
+
+		#output = l4_points.view(batch_size, 1920)
+		output = l4_points
+		return l2_xyz, output # (batch_size, 1920)
 
 # num_center_point: number of generated center points from dense output
 class FeatureExtractor_2(nn.Module):
-    def __init__(self, num_points):
-        super(FeatureExtractor_2, self).__init__()
-        self.num_points = num_points
-        self.conv1 = torch.nn.Conv2d(1, 64, (1, 3))
-        self.conv2 = torch.nn.Conv2d(64, 64, 1)
-        self.conv3 = torch.nn.Conv2d(64, 128, 1)
-        self.conv4 = torch.nn.Conv2d(128, 256, 1)
-        self.conv5 = torch.nn.Conv2d(256, 512, 1)
-        self.conv6 = nn.Conv2d(512, 1024, 1)
-        self.maxpool = nn.MaxPool2d((self.num_points, 1), 1)
+	def __init__(self, num_points):
+		super(FeatureExtractor_2, self).__init__()
+		self.num_points = num_points
+		self.conv1 = torch.nn.Conv2d(1, 64, (1, 3))
+		self.conv2 = torch.nn.Conv2d(64, 64, 1)
+		self.conv3 = torch.nn.Conv2d(64, 128, 1)
+		self.conv4 = torch.nn.Conv2d(128, 256, 1)
+		self.conv5 = torch.nn.Conv2d(256, 512, 1)
+		self.conv6 = nn.Conv2d(512, 1024, 1)
+		self.maxpool = nn.MaxPool2d((self.num_points, 1), 1)
 
-        self.bn1 = nn.BatchNorm2d(64)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.bn3 = nn.BatchNorm2d(128)
-        self.bn4 = nn.BatchNorm2d(256)
-        self.bn5 = nn.BatchNorm2d(512)
-        self.bn6 = nn.BatchNorm2d(1024)
+		self.bn1 = nn.BatchNorm2d(64)
+		self.bn2 = nn.BatchNorm2d(64)
+		self.bn3 = nn.BatchNorm2d(128)
+		self.bn4 = nn.BatchNorm2d(256)
+		self.bn5 = nn.BatchNorm2d(512)
+		self.bn6 = nn.BatchNorm2d(1024)
 
-        self.fc1 = nn.Linear(1920, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 128)
+		self.fc1 = nn.Linear(1920, 512)
+		self.fc2 = nn.Linear(512, 256)
+		self.fc3 = nn.Linear(256, 128)
 
-        self.fcbn1 = nn.BatchNorm1d(512)
-        self.fcbn2 = nn.BatchNorm1d(256)
-        self.fcbn3 = nn.BatchNorm1d(128)
+		self.fcbn1 = nn.BatchNorm1d(512)
+		self.fcbn2 = nn.BatchNorm1d(256)
+		self.fcbn3 = nn.BatchNorm1d(128)
 
 
-    def forward(self, x): # input size = [batch_size, num_center_point, 3]
-        x = torch.unsqueeze(x, 1)
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x_128 = F.relu(self.bn3(self.conv3(x)))
-        x_256 = F.relu(self.bn4(self.conv4(x_128)))
-        x_512 = F.relu(self.bn5(self.conv5(x_256)))
-        x_1024 = F.relu(self.bn6(self.conv6(x_512)))
-        x_128 = torch.squeeze(self.maxpool(x_128), 2)
-        x_256 = torch.squeeze(self.maxpool(x_256), 2)
-        x_512 = torch.squeeze(self.maxpool(x_512), 2)
-        x_1024 = torch.squeeze(self.maxpool(x_1024), 2)
-        multi_layers = [x_1024, x_512, x_256, x_128]
+	def forward(self, x): # input size = [batch_size, 3, num_center_point]
+		#print('input size', x.shape)
+		x = x.permute(0, 2, 1) # [batch_size, num_center_point, 3]
+		x = torch.unsqueeze(x, 1)
+		x = F.relu(self.bn1(self.conv1(x)))
+		x = F.relu(self.bn2(self.conv2(x)))
+		x_128 = F.relu(self.bn3(self.conv3(x)))
+		#print(x_128.shape)
+		x_256 = F.relu(self.bn4(self.conv4(x_128)))
+		#print(x_256.shape)
+		x_512 = F.relu(self.bn5(self.conv5(x_256)))
+		#print(x_512.shape)
+		x_1024 = F.relu(self.bn6(self.conv6(x_512))) 
+		#print(x_1024.shape)
 
-        output = torch.cat(multi_layers, 1) # 1024+512+256+128 = 1920
-        return output # (batch_size, 1920)
+		x_128 = torch.squeeze(self.maxpool(x_128), 2) 
+		x_256 = torch.squeeze(self.maxpool(x_256), 2)
+		x_512 = torch.squeeze(self.maxpool(x_512), 2)
+		x_1024 = torch.squeeze(self.maxpool(x_1024), 2)
+		multi_layers = [x_1024, x_512, x_256, x_128]
 
-# DISCRIMINATOR UPDATES TO BE WRITTEN TO MAIN
-"""
-criterion = torch.nn.BCEWithLogitsLoss().to(device)
-optimizerD = torch.optim.Adam(D_net.parameters(), lr=0.0001, betas=(0.9, 0.999),eps=1e-05, weight_decay=0.001)
+		output = torch.cat(multi_layers, 1) # 1024+512+256+128 = 1920
+		#print(output.shape)
+		return output # (batch_size, 1920, 1)
 
-real_out = D_net(real_center) 						# assume real_center in shape [batch_size, num_center_point, 3]
-errorD_real = criterion(real_out, real_label) 		# real_out and real_label in shape [batch_size, 1]
-errorD_real.backward()
-
-fake_out = D_net(fake_center)
-errorD_fake = criterion(fake_out, fake_label)
-errorD_fake.backward()
-errorD = errorD_real + errorD_fake
-optimizerD.step()
-"""
---
+++ train.py
@@ -0,0 +1,287 @@
+import os
+import sys
+import argparse
+import random
+import torch
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.utils.data
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import utils
+from utils import PointLoss
+from utils import distance_squre
+import data_utils as d_utils
+import ModelNet40Loader
+import shapenet_part_loader
+from Decoder import Decoder
+from D_net import D_net
+torch.backends.cudnn.enabled = False
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataroot',  default='dataset/train', help='path to dataset')
+parser.add_argument('--workers', type=int,default=2, help='number of data loading workers')
+parser.add_argument('--batchSize', type=int, default=12, help='input batch size')
+parser.add_argument('--pnum', type=int, default=2048, help='the point number of a sample')
+parser.add_argument('--crop_point_num',type=int,default=512,help='0 means do not use else use with this weight')
+parser.add_argument('--niter', type=int, default=1, help='number of epochs to train for')
+parser.add_argument('--weight_decay', type=float, default=0.001)
+parser.add_argument('--learning_rate', default=0.0002, type=float, help='learning rate in training')
+parser.add_argument('--beta1', type=float, default=0.9, help='beta1 for adam. default=0.9')
+parser.add_argument('--cuda', type = bool, default = False, help='enables cuda')
+parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
+parser.add_argument('--manualSeed', type=int, help='manual seed')
+parser.add_argument('--drop',type=float,default=0.2) # not sure what this does
+parser.add_argument('--num_scales',type=int,default=2,help='number of scales')
+parser.add_argument('--point_scales_list',type=list,default=[2048,512],help='number of points in each scales')
+parser.add_argument('--each_scales_size',type=int,default=1,help='each scales size')
+parser.add_argument('--wtl2',type=float,default=0.95,help='0 means do not use else use with this weight')
+parser.add_argument('--cropmethod', default = 'random_center', help = 'random|center|random_center')
+parser.add_argument('--netG', default='', help="put in gen_net.pth location to continue training)")
+parser.add_argument('--netD', default='', help="put in dis_net.pth location to continue training)")
+opt = parser.parse_args()
+# print(opt)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+USE_CUDA = True
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+gen_net = Decoder(opt.point_scales_list[0], opt.crop_point_num)
+dis_net = D_net(opt.crop_point_num)
+cudnn.benchmark = True # faster runtime
+resume_epoch = 0
+
+print(dis_net)
+print(gen_net)
+
+def weights_init_normal(m):
+	classname = m.__class__.__name__
+	if classname.find("Conv2d") != -1:
+		torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+	elif classname.find("Conv1d") != -1:
+		torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+	elif classname.find("BatchNorm2d") != -1:
+		torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+		torch.nn.init.constant_(m.bias.data, 0.0)
+	elif classname.find("BatchNorm1d") != -1:
+		torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+		torch.nn.init.constant_(m.bias.data, 0.0) 
+
+# initialize generator and discriminator weights and device
+if USE_CUDA:
+	print("Using", torch.cuda.device_count(), "GPUs")
+	gen_net = torch.nn.DataParallel(gen_net)
+	gen_net.to(device) 
+	gen_net.apply(weights_init_normal)
+	dis_net = torch.nn.DataParallel(dis_net)
+	dis_net.to(device)
+	dis_net.apply(weights_init_normal)
+
+if opt.netG != '' :
+	gen_net.load_state_dict(torch.load(opt.netG,map_location=lambda storage, location: storage)['state_dict'])
+	resume_epoch = torch.load(opt.netG)['epoch']
+	#print('G loaded')
+if opt.netD != '' :
+	dis_net.load_state_dict(torch.load(opt.netD,map_location=lambda storage, location: storage)['state_dict'])
+	resume_epoch = torch.load(opt.netD)['epoch']
+	#print('D loaded')
+#print(resume_epoch)
+
+
+# seeding for cropping
+# manual seed and send seed to cuda
+if opt.manualSeed is None:
+	opt.manualSeed = random.randint(1, 10000)
+print("Random Seed: ", opt.manualSeed)
+random.seed(opt.manualSeed)
+torch.manual_seed(opt.manualSeed)
+if opt.cuda:
+	torch.cuda.manual_seed_all(opt.manualSeed)
+
+# define transforms for point cloud data
+transforms = transforms.Compose([d_utils.PointcloudToTensor(),])
+
+train_set = shapenet_part_loader.PartDataset( root='./dataset/shapenetcore_partanno_segmentation_benchmark_v0/',classification=True, class_choice=None, npoints=opt.pnum, split='train')
+assert train_set
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=opt.batchSize,
+										 shuffle=True,num_workers = int(opt.workers))
+
+test_set = shapenet_part_loader.PartDataset( root='./dataset/shapenetcore_partanno_segmentation_benchmark_v0/',classification=True, class_choice=None, npoints=opt.pnum, split='test')
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=opt.batchSize,
+										 shuffle=True,num_workers = int(opt.workers))
+
+criterion = torch.nn.BCEWithLogitsLoss().to(device)
+criterion_PointLoss = PointLoss().to(device)
+real_label = 1
+fake_label = 0
+
+# setup optimizer
+optimizerD = torch.optim.Adam(dis_net.parameters(), lr=0.0001,betas=(0.9, 0.999),eps=1e-05,weight_decay=opt.weight_decay)
+schedulerD = torch.optim.lr_scheduler.StepLR(optimizerD, step_size=40, gamma=0.2)
+optimizerG = torch.optim.Adam(gen_net.parameters(), lr=0.0001,betas=(0.9, 0.999),eps=1e-05 ,weight_decay=opt.weight_decay)
+schedulerG = torch.optim.lr_scheduler.StepLR(optimizerG, step_size=40, gamma=0.2)
+
+crop_point_num = int(opt.crop_point_num)
+input_cropped1 = torch.FloatTensor(opt.batchSize, opt.pnum, 3)
+label = torch.FloatTensor(opt.batchSize)
+
+num_batch = len(train_set) / opt.batchSize
+
+# train with generator and discriminator
+for epoch in range(resume_epoch, opt.niter):
+	if epoch<30:
+		lam1 = 0.01
+		lam2 = 0.02
+	elif epoch<80:
+		lam1 = 0.05
+		lam2 = 0.1
+	else:
+		lam1 = 0.1
+		lam2 = 0.2
+	
+	for i, data in enumerate(train_loader):
+		real_point, target = data
+		batch_size = real_point.size()[0] # real_point.shape = [24, 2024, 3]
+		real_center = torch.FloatTensor(batch_size, 1, opt.crop_point_num, 3)       
+		input_cropped1 = torch.FloatTensor(batch_size, opt.pnum, 3)
+		input_cropped1 = input_cropped1.data.copy_(real_point)
+		real_point = torch.unsqueeze(real_point, 1)
+		input_cropped1 = torch.unsqueeze(input_cropped1,1) # input_cropped1.shape = [24, 1, 2024, 3]
+		p_origin = [0,0,0]
+
+		if opt.cropmethod == 'random_center':
+			# set viewpoints
+			choice = [torch.Tensor([1,0,0]),torch.Tensor([0,0,1]),torch.Tensor([1,0,1]),torch.Tensor([-1,0,0]),torch.Tensor([-1,1,0])]
+			for m in range(batch_size):
+				index = random.sample(choice,1)#Random choose one of the viewpoint
+				distance_list = []
+				p_center = index[0]
+				for n in range(opt.pnum):
+					distance_list.append(distance_squre(real_point[m,0,n],p_center))
+				distance_order = sorted(enumerate(distance_list), key  = lambda x:x[1])
+				
+				for sp in range(opt.crop_point_num):
+					input_cropped1.data[m,0,distance_order[sp][0]] = torch.FloatTensor([0,0,0])
+					real_center.data[m,0,sp] = real_point[m,0,distance_order[sp][0]]
+		label.resize_([batch_size,1]).fill_(real_label)
+		real_point = real_point.to(device) # real_point.shape = [24, 1, 2048, 3]
+		real_center = real_center.to(device) # real_center.shape = [24, 1, 512, 3]
+		input_cropped1 = input_cropped1.to(device) # input_cropped1.shape = [24, 1, 2048, 3]
+		label = label.to(device) # real label construction done
+		
+		# obtain data for the two channels
+		real_center = Variable(real_center,requires_grad=True)
+		real_center = torch.squeeze(real_center,1) # [24, 512, 3]
+		real_center_key1_idx = utils.farthest_point_sample(real_center, 128,RAN = False)
+		real_center_key1 = utils.index_points(real_center,real_center_key1_idx)
+		real_center_key1 = Variable(real_center_key1,requires_grad=True) # [24, 128, 3]
+
+		input_cropped1 = torch.squeeze(input_cropped1,1)
+		input_cropped1 = Variable(input_cropped1,requires_grad=True) # [24, 2048, 3]
+
+		input_cropped2_idx = utils.farthest_point_sample(input_cropped1, opt.point_scales_list[1],RAN = True)
+		input_cropped2 = utils.index_points(input_cropped1,input_cropped2_idx)
+
+		input_cropped2 = Variable(input_cropped2,requires_grad=True)
+		input_cropped2 = input_cropped2.to(device) # [24, 512, 3]
+		input_cropped = [input_cropped1, input_cropped2] # make sure if inputs are 2048 and 512
+		gen_net = gen_net.train()
+		dis_net = dis_net.train()
+
+		# update discriminator
+		dis_net.zero_grad()
+		#print('real center shape', real_center.shape)
+		real_center = torch.unsqueeze(real_center,1)   
+		real_out = dis_net(real_center)
+		#print('real label shape', label.shape)
+		dis_err_real = criterion(real_out, label)
+		dis_err_real.backward()
+
+		fake_center1,fake_fine = gen_net(input_cropped1)
+
+		fake_fine = torch.unsqueeze(fake_fine,1)
+		label.data.fill_(fake_label)
+		fake_out = dis_net(fake_fine.detach())
+		dis_err_fake = criterion(fake_out, label)
+		dis_err_fake.backward()
+		dis_err = dis_err_real + dis_err_fake
+		optimizerD.step()
+
+		# update generator objective max(log(D(G(z))))
+		gen_net.zero_grad()
+		label.data.fill_(real_label)
+		fake_out = dis_net(fake_fine)
+		errG_D = criterion(fake_out, label) # discriminator loss of fake points
+		errG_l2 = 0
+   
+		errG_l2 = criterion_PointLoss(torch.squeeze(fake_fine,1),torch.squeeze(real_center,1))\
+		+lam1*criterion_PointLoss(fake_center1,real_center_key1) # generator loss(AE loss)
+		
+		errG = (1-opt.wtl2) * errG_D + opt.wtl2 * errG_l2 # total adversarial loss
+		#print('errG', errG)
+		errG.backward()
+		optimizerG.step()
+		print('Epoch[%d/%d] Batch[%d/%d] D_loss: %.4f G_loss: %.4f errG_D: %.4f errG_l2: %.4f'
+			  % (epoch, opt.niter, i, len(train_loader), 
+				 dis_err.data, errG, errG_D.data, errG_l2))
+		f=open('loss_PFNet.txt','a')
+		f.write('\n'+'Epoch[%d/%d] Batch[%d/%d] D_loss: %.4f G_loss: %.4f errG_D: %.4f errG_l2: %.4f'
+			  % (epoch, opt.niter, i, len(train_loader), 
+				 dis_err.data, errG, errG_D.data, errG_l2))
+	
+	# start of testing
+	f=open('loss_PFNet.txt','a')
+	if epoch % 10 == 0:
+		print('After, ',epoch,'-th batch')
+		for i, data in enumerate(test_loader):
+			real_point, target = data
+			batch_size = real_point.size()[0]
+			real_center = torch.FloatTensor(batch_size, 1, opt.crop_point_num, 3)
+			input_cropped1 = torch.FloatTensor(batch_size, opt.pnum, 3)
+			input_cropped1 = input_cropped1.data.copy_(real_point)
+
+			real_point = torch.unsqueeze(real_point, 1)
+			input_cropped1 = torch.unsqueeze(input_cropped1,1)
+
+			p_origin = [0,0,0]
+
+			if opt.cropmethod == 'random_center':
+				choice = [torch.Tensor([1,0,0]),torch.Tensor([0,0,1]),torch.Tensor([1,0,1]),torch.Tensor([-1,0,0]),torch.Tensor([-1,1,0])]
+				
+				for m in range(batch_size):
+					index = random.sample(choice,1)
+					distance_list = []
+					p_center = index[0]
+					for n in range(opt.pnum):
+						distance_list.append(distance_squre(real_point[m,0,n],p_center))
+					distance_order = sorted(enumerate(distance_list), key  = lambda x:x[1])                         
+					for sp in range(opt.crop_point_num):
+						input_cropped1.data[m,0,distance_order[sp][0]] = torch.FloatTensor([0,0,0])
+						real_center.data[m,0,sp] = real_point[m,0,distance_order[sp][0]]  
+			real_center = real_center.to(device)
+			real_center = torch.squeeze(real_center,1)
+			input_cropped1 = input_cropped1.to(device) 
+			input_cropped1 = torch.squeeze(input_cropped1,1)
+			input_cropped2_idx = utils.farthest_point_sample(input_cropped1,opt.point_scales_list[1],RAN = True)
+			input_cropped2 = utils.index_points(input_cropped1,input_cropped2_idx)
+
+			input_cropped1 = Variable(input_cropped1,requires_grad = False)
+			input_cropped2 = Variable(input_cropped2,requires_grad = False)
+			input_cropped2 = input_cropped2.to(device)
+			input_cropped = [input_cropped1,input_cropped2]
+			gen_net.eval()
+			fake_center1, fake_fine = gen_net(input_cropped1)
+			CD_loss = criterion_PointLoss(torch.squeeze(fake_fine,1),torch.squeeze(real_center,1))
+			print('test CD loss: %.4f'%(CD_loss))
+			f.write('\n'+'test result:  %.4f'%(CD_loss))
+			break
+		f.close()
+		schedulerD.step()
+		schedulerG.step()
+		if epoch% 10 == 0:   
+			torch.save({'epoch':epoch+1,
+						'state_dict':gen_net.state_dict()},
+						'Trained_Model/gen_net'+str(epoch)+'.pth')
+			torch.save({'epoch':epoch+1,
+						'state_dict':dis_net.state_dict()},
+						'Trained_Model/dis_net'+str(epoch)+'.pth')
+
+print('done')
