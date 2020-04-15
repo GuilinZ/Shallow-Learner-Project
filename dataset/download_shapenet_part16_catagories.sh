dbfb9a4e21eaf5c64ec5327ee807bb69646e5de6
liuyuex97 <liuyuex@andrew.cmu.edu>
2020-04-14 21:17:51 -0400

    Create download_shapenet_part16_catagories.sh

---
+++ dataset/download_shapenet_part16_catagories.sh
@@ -0,0 +1,11 @@
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+cd $SCRIPTPATH/
+wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip --no-check-certificate
+mkdir -p shapenet_part
+mv shapenetcore_partanno_segmentation_benchmark_v0.zip shapenet_part/
+cd $SCRIPTPATH/shapenet_part/
+unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
+rm shapenetcore_partanno_segmentation_benchmark_v0.zip
+cd $SCRIPTPATH
